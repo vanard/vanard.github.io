c92673da23bd047448c65027bad046a5010a6b50
vanard <vianixa34@gmail.com>
2017-05-24 16:39:42 +0700

    add files via upload, second commit
    portfolio

---
+++ js/script.js
@@ -0,0 +1,54 @@
+//
+$('.page').on('click',function(e) {
+
+	var href = $(this).attr('href');
+	var elemenHref = $(href);
+
+	$('body').animate({
+		scrollTop: elemenHref.offset().top-50}, 1350,'easeInOutExpo');
+
+	e.preventDefault();
+
+});
+
+
+//paralax
+
+//about
+$(window).on('load', function(){
+	$('.pLeft').addClass('pAppear');
+	$('.pRight').addClass('pAppear');
+});
+
+
+//portfolio
+$(window).scroll(function(){
+	var wScroll = $(this).scrollTop();
+
+	//jumbotron
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+	});
+
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+	});
+
+	$('.jumbotron p').css({
+		'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+	});
+
+	//portfolio
+	if (wScroll > $('.portfolio').offset().top-250) {
+		$('.portfolio .thumbnail').each(function(i){
+			setTimeout(function(){
+				$('.portfolio .thumbnail').eq(i).addClass('appear')
+			}, 250*(i+1));
+		});
+		//$('.portfolio .thumbnail').addClass('appear');
+	}
+
+
+});
+
+//
